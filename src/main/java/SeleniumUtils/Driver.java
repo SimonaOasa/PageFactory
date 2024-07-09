018c37e367a42befdef937388368c5988c279b3f
simona oasa <simona.oasa@outlook.com>
2024-07-09 18:03:55 +0300

    curs36 final

--- src/main/java/SeleniumUtils/Driver.java
+++ src/main/java/SeleniumUtils/Driver.java
@@ -2,8 +2,12 @@ package SeleniumUtils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Driver {
 
@@ -15,16 +19,17 @@ public class Driver {
 
         if(browser.equalsIgnoreCase("chrome")) {
         	
-        	driver.set(new ChromeDriver());
+        	driver.set(new ChromeDriver(getChromeOptions()));
         	driver.get();
         	
         }else if(browser.equalsIgnoreCase("firefox")) {
         	
-        	driver.set(new FirefoxDriver()); 
+        	driver.set(new FirefoxDriver(getFirefoxOptions())); 
         	driver.get();
         	
         }else if(browser.equalsIgnoreCase("edge")) {
-        	driver.set(new EdgeDriver());
+        	
+        	driver.set(new EdgeDriver(getEdgeOptions()));
         	driver.get();
         }
 		
@@ -33,6 +38,47 @@ public class Driver {
 		return driver.get();
 		
 				
+	}
+	
+	public static FirefoxOptions getFirefoxOptions() {
+		/*FirefoxBinary binary = new FirefoxBinary();
+		binary.addCommandLineOptions("--headless");
+		FirefoxOptions option =  new FirefoxOptions();
+		option.setBinary(binary); */
+		
+		FirefoxOptions option = new FirefoxOptions();
+		option.addArguments("--headless");
+		
+		return option;
+		
+	}
+	
+	public static ChromeOptions getChromeOptions() {
+		/*FirefoxBinary binary = new FirefoxBinary();
+		binary.addCommandLineOptions("--headless");
+		FirefoxOptions option =  new FirefoxOptions();
+		option.setBinary(binary); */
+		
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--window-size=1920,1080");
+		option.addArguments("--headless");
+		
+		return option;
+		
+	}
+	
+	public static EdgeOptions getEdgeOptions() {
+		/*FirefoxBinary binary = new FirefoxBinary();
+		binary.addCommandLineOptions("--headless");
+		FirefoxOptions option =  new FirefoxOptions();
+		option.setBinary(binary); */
+		
+		EdgeOptions option = new EdgeOptions();
+		option.addArguments("--window-size=1920,1080");
+		option.addArguments("--headless");
+		
+		return option;
+		
 	}
 	
 }
